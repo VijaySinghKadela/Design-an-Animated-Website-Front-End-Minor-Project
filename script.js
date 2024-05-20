3f8061edb1b3454c4f163715429867a026bd2973
VinaySinghKadela <132544545+VijaySinghKadela@users.noreply.github.com>
2024-05-20 18:23:38 +0530

    added

--- script.js
+++ script.js
@@ -1,53 +1,77 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
 let videocon = document.querySelector("#video-cantainer");
 let playbtn = document.querySelector("#play");
 
-function videoconAnimation(){
-    videocon.addEventListener("mouseenter", function () {
-        gsap.to(playbtn,{
-            opacity:1,
-            scale:1,
-        })
+function videoconAnimation() {
+  videocon.addEventListener("mouseenter", function () {
+    gsap.to(playbtn, {
+      opacity: 1,
+      scale: 1,
     });
-    
-    videocon.addEventListener('mouseleave',function(){
-        gsap.to(playbtn,{
-            opacity:0,
-            scale:0,
-        })
-    })
-    
-    videocon.addEventListener('mousemove',function(dets){
-        gsap.to(playbtn,{
-            top: dets.y-50,
-            left: dets.x-50,
-        })
-    })
+  });
+
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(playbtn, {
+      opacity: 0,
+      scale: 0,
+    });
+  });
+
+  videocon.addEventListener("mousemove", function (dets) {
+    gsap.to(playbtn, {
+      top: dets.y,
+      left: dets.x,
+    });
+  });
 }
-videoconAnimation()
+videoconAnimation();
 
-function loadingAnimation(){
-    let mainHeadline = document.querySelector('#page1 h1')
+function loadingAnimation() {
+  let mainHeadline = document.querySelector("#page1 h1");
 
-gasp.from(mainHeadline,{
+  gsap.from(mainHeadline, {
     y: 100,
     opacity: 0,
-    delay:0.5,
-    duration:0.9,
+    delay: 0.5,
+    duration: 0.9,
     stagger: 0.3,
-})
+  });
 
-gasp.from(videocon,{
+  gsap.from(videocon, {
     sclae: 0.9,
     opacity: 0,
-    delay:1.3,
-    duration:0.5,
-
-})
+    delay: 1.3,
+    duration: 0.5,
+  });
 }
-loadingAnimation()
+loadingAnimation();
+
+let body = document.querySelector("body");
+
+body.addEventListener("mousemove", function (dets) {
+  gsap.to("#cursor", {
+    left: dets.x,
+    top: dets.y,
+  });
+});
+
+let allChild = document.querySelectorAll(".child");
+
+allChild.forEach(function (elem) {
+  elem.addEventListener("mouseenter", function () {
+    gsap.to("#cursor", {
+      transform: "translate(-50%,-50%) scale(1)",
+    });
+  });
+  elem.addEventListener("mouseleave", function () {
+    gsap.to("#cursor", {
+      transform: "translate(-50%,-50%) scale(0)",
+    });
+  });
+});
+
 
