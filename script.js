af4948fde9d6bd2310cdf8ca71c9a8bca4d797b6
VinaySinghKadela <132544545+VijaySinghKadela@users.noreply.github.com>
2024-05-20 16:24:37 +0530

    added

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-
-// importing an gasp form site
-import { gsap } from "gsap";
-import { CustomEase } from "gsap/CustomEase";
-import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";
-import { Flip } from "gsap/Flip";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Observer } from "gsap/Observer";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { Draggable } from "gsap/Draggable";
-import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import { EaselPlugin } from "gsap/EaselPlugin";
-import { PixiPlugin } from "gsap/PixiPlugin";
-import { TextPlugin } from "gsap/TextPlugin";ezx
-
-
-gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin,RoughEase,ExpoScaleEase,SlowMo,CustomEase);
-
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
 
 let videocon = document.querySelector("#video-cantainer");
 let playbtn = document.querySelector("#play");
